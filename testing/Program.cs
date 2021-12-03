2e46603f70a8e148ccbaa499cd01ce9fa84719be
User <User@DESKTOP-8VGVODD>
2021-12-03 22:13:54 +0700

    Add project files.

---
+++ testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world !");
+            Console.WriteLine("kfdjsalfjas");
+            Console.ReadKey();
+        }
+    }
+}
